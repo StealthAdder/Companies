9807885a5d4ab45b636a2c75af4c55ba3bae507a
Hariharan Parthiban <hariharanp2435@gmail.com>
2020-06-20 03:43:21 +0530

    php clean up - 3:42AM after js validation.
    Signed-off-by: Hariharan Parthiban <hariharanp2435@gmail.com>

--- php/contact.php
+++ php/contact.php
@@ -19,58 +19,14 @@
             <h2 id="gint">Queries</h2>
             <div id="frm">
                 <form action="/proc_pages/t_process.php" method="post" autocomplete="off" onsubmit="return validation()">
-                <span id="name">Full Name :<input type="text" name="flName"></span><span class="error">*<?php echo $_POST["flNameErr"]?></span><br>
-                <span id="mail">Maild Id : <input type="text" name="mail"></span><span class="error">*<?php echo $_POST["mailErr"]?></span><br>
-                <span id="phone">Phone No: <input type="text" name="phoneNo"></span><span class="error">*<?php echo $_POST["phoneNoErr"]?></span><br>
+                <span id="name">Full Name :<input type="text" name="flName"></span><br>
+                <span id="mail">Maild Id : <input type="text" name="mail"></span><br>
+                <span id="phone">Phone No: <input type="text" name="phoneNo"></span><br>
                 <span id="lym">Leave your message: </span><br>
                     <textarea name="msg" id="msg" cols="80" rows="8"></textarea>
-                    <span class="error">*<?php echo $_POST["msgErr"]?></span>
                 <br>
                 <input type="submit" value="Send" id="sumbit">
                 </form>
-                <?php
-                $flName = $mail = $phoneNo = $msg = "";
-                $flNameErr = $mailErr = $phoneNoErr = $msgErr = "";
-
-                //
-                if ($_SERVER["REQUESTED METHOD"] == "POST") {
-                    
-                    if ($flName){       //if (empty($_POST["flName"]))
-                        $flNameErr = "Required";
-                    }
-                    else {
-                        $flName = test_input($_POST["flName"]);
-                    }
-
-                    if (empty($_POST["mail"])){
-                        $mailErr = "Required";
-                    }
-                    else {
-                        $mail = test_input($_POST["mail"]);
-                    }
-                    
-                    if (empty($_POST["phoneNo"])){
-                        $phoneNoErr = "Required";
-                    }
-                    else {
-                        $phoneNo = test_input($_POST["phoneNo"]);
-                    }
-
-                    if (empty($_POST["msg"])){
-                        $msgErr = "Required";
-                    }
-                    else {
-                        $msg = test_input($_POST["msg"]);
-                    }
-                }
-
-                function test_input($data) {
-                    $data = trim($data);
-                    $data = stripslashes($data);
-                    $data = htmlspecialchars($data);
-                    return $data;
-                }
-            ?>
             </div>
         </div>
 
