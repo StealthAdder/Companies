b880ac6127fd133739868aeeebecbe99d052a339
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-06-20 00:22:51 +0530

    form validayion
    Or just use require in html

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -1,7 +1,24 @@
 function validation(){
-    let x =document.forms["flName"]["mail"]["phoneNo"]["msg"].value;
-    if(x === ""){
+    // let x =document.forms["RegForm"]["flName"]["mail"]["phoneNo"]["msg"].value;
+    let name =document.forms["RegForm"]["flName"];
+    let mail =document.forms["RegForm"]["mail"];
+    let phone =document.forms["RegForm"]["phoneNo"];
+    let msg =document.forms["RegForm"]["msg"];
+
+    if(name.value == ""){
         alert("All fields are required");
+        return false;
+    }
+    if(mail.value == ""){
+        alert("Mail is required");
+        return false;
+    }
+    if(phone.value == ""){
+        alert("Phone number is required");
+        return false;
+    }
+    if(msg.value == ""){
+        alert("Write something");
         return false;
     }
 }
