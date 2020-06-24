7aa421e133b2d86aadaa789f30b94d234db7d4a5
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-06-24 23:13:07 +0530

    Live Time

--- javascripts/add-content.js
+++ javascripts/add-content.js
@@ -1,29 +1,30 @@
-let today = new Date();
-let hourNow = today.getHours();
-//let timeNow = today.getTime();
-let timeNow = today.getHours()+":"+ today.getMinutes()+":"+today.getSeconds();
-let greetings;
-
-if (hourNow > 18) {
-    greetings = 'Hey, Good evening!';
-} else if (hourNow > 12) {
-    greetings = 'Hey, Good afternoon!';
-} else if (hourNow > 0) {
-    greetings = 'Hey, Good morning!';
-} else {
-    greetings = 'Welcome the chossen one';
-}
-
 // document.write('<h3 id="greet">' + greetings + '</h3>');
 // document.write('<h4 id="times">' + timeNow + " milliseconds have passed since January 1, 1970" + '</h1>');
-function updateMessage(){
-    let el =document.getElementById('greetings');
+function updateMessage() {
+    let today = new Date();
+    let hourNow = today.getHours();
+    let ampm = hourNow >= 12 ? 'PM' : 'AM';
+    let hour = hourNow % 12;
+    //let timeNow = today.getTime();
+    let timeNow = hour + ":" + today.getMinutes() + ":" + today.getSeconds() + " " + ampm;
+    let greetings;
+
+    if (hourNow > 18) {
+        greetings = 'Hey, Good evening!';
+    } else if (hourNow > 12) {
+        greetings = 'Hey, Good afternoon!';
+    } else if (hourNow > 0) {
+        greetings = 'Hey, Good morning!';
+    } else {
+        greetings = 'Welcome the chossen one';
+    }
+
+    let el = document.getElementById('greetings');
     let el2 = document.getElementById('Time');
 
     el.textContent = greetings;
-    el2.textContent = timeNow ;
+    el2.textContent = timeNow;
 
 }
 
-updateMessage();
-
+setInterval(updateMessage, 1000);
