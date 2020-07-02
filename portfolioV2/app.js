be6bfde33513f25923c12734bf1fdc11920c8060
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-07-02 13:43:13 +0530

    added contact succes page

--- portfolioV2/app.js
+++ portfolioV2/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })   // body parser middleware
 
 app.set('view engine', 'ejs');
 
@@ -14,11 +17,16 @@ app.get('/contact', function (req, res) {
     res.render('contact');
 })
 
+app.post('/contact', urlencodedParser, function (req, res) {
+    console.log(req.body);  // urlencodedParser gives access to the body property
+    res.render('contact-success', { data: req.body });
+})
+
 app.get('/profile/:name', function (req, res) {
     res.render('profile', { person: req.params.name });
 })
 // Color Flip Page
-app.get('/RandomColor',function(req,res){
+app.get('/RandomColor', function (req, res) {
     res.render('colorflip');
 })
 app.listen(3000);
